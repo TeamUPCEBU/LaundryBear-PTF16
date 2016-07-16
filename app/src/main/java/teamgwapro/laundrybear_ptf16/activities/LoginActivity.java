8d7f6f9c770fac9bf3f2f2d11b7a8c9cead5325f
tpen-rey <avila.stephenrey@gmail.com>
2016-07-16 15:27:16 +0800

    Add view shops

--- app/src/main/java/teamgwapro/laundrybear_ptf16/activities/LoginActivity.java
+++ app/src/main/java/teamgwapro/laundrybear_ptf16/activities/LoginActivity.java
@@ -1,6 +1,7 @@
 package teamgwapro.laundrybear_ptf16.activities;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
